2346ad57321a39d47c15d6d26f15e5dc560cbcf4
MashV9 <mashvv9@gmail.com>
2025-08-28 16:07:55 -0700

    Platform hit detection for jungle escape game.

---
+++ DetectFalsePlatforms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    bool hit;
+    void Update()
+    {
+        hit = Physics.Raycast(transform.localPosition, transform.forward, 1.5f);
+        Debug.DrawRay(transform.position, Vector3.forward * .15f, Color.red, 1 << 8);
+        if (hit == true)
+        {
+            Debug.LogWarning("Be careful!");
+        }
+
+        else
+        {
+            Debug.Log("All clear!");
+        }
+        
+    }   
+}
